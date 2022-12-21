89b42a1e6a4666c3c5d157856b18d6bb3c6b9520
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2022-12-21 19:58:22 +0300

    insert done

--- DinamicMemory/Source.cpp
+++ DinamicMemory/Source.cpp
@@ -7,7 +7,9 @@ void Print(int* rr, const int n);
 
 int* push_back(int* arr, int& n, int value);
 int* push_front(int* arr, int& n, int value);
-
+int* insert(int* arr, int& n, int value, int index);
+int* pop_back(int* arr, int& n);
+int* pop_front(int* arr, int& n);
 
 void main()
 {
@@ -28,6 +30,17 @@ void main()
 	arr = push_front(arr, n, value);
 	Print(arr, n);
 
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	int index;
+	cout << "¬ведите индекс добавл€емого значени€: "; cin >> index;
+	arr = insert(arr, n, value, index);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
+	Print(arr, n);
 
 	delete[] arr;
 	//Memory leak
@@ -88,4 +101,38 @@ int* push_front(int* arr, int& n, int value)
 	arr[0] = value;
 	n++;
 	return arr;
+}
+
+int* insert(int* arr, int& n, int value, int index)
+{
+	int* buffer = new int[n + 1];
+	//for (int i = 0; i < index; i++)buffer[i] = arr[i];
+	//for (int i = index; i<n; i++)buffer[i + 1] = arr[i];
+	for (int i = 0; i < n; i++)
+	{
+		//if (i < index)buffer[i] = arr[i];else buffer[i + 1] = arr[i]; 
+		//(i < index ? buffer[i] : buffer[i + 1]) = arr[i];
+		buffer[i < index ? i : i + 1] = arr[i];
+	}
+	buffer[index] = value;
+	delete[] arr;
+	arr = buffer; 
+	n++;
+	return arr;
+}
+
+int* pop_back(int* arr, int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+ 
+int* pop_front(int* arr, int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i + 1];
+	delete[] arr;
+	return buffer;
 }
